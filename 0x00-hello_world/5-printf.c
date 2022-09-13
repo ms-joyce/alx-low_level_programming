9b3a79d1d7bcf763bfef7af45f7da6462507c2a6
ms-joyce <itsjoyce2@gmail.com>
2022-09-12 19:55:30 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,10 @@
 
 /**
  *
- * main - Entry point
+ * main - Prints given text
+ *
+ * Description: using the main function
+ * this program prints "with proper grammar, but the outcome is a piece of art,"
  *
  * Return: Always 0 (Success)
  */
