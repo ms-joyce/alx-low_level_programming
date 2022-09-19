208ed7c3906cf5f4879178095c8cc185e2400b31
ms-joyce <itsjoyce2@gmail.com>
2022-09-19 01:46:34 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints hexadecimal numbers
+ *
+ * Returrn: Always (Success)
+ */
+int main(void)
+{
+	char number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+		putchar(number);
+	}
+	for (number = 'a'; number <= 'f'; number++)
+	{
+		putchar(number);
+	}
+	putchar('\n');
+
+	return (0);
+}
