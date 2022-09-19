48da45c3696ae857217fed408ba479e6cee8748d
ms-joyce <itsjoyce2@gmail.com>
2022-09-19 00:46:09 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Print single digits from 0 to 9
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		putchar(digit);
+	}
+
+	putchar('\n')
+	
+	return (0);
+}
