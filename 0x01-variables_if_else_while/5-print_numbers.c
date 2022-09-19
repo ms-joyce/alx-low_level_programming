5f65a3749958df778786c06fb7c80391b74eadd8
ms-joyce <itsjoyce2@gmail.com>
2022-09-19 00:20:27 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Print single digits from 0 to 9
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		printf("%d, digit);
+		printf("\n");
+	}
+
+	retun (0);
+}
