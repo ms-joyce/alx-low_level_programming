d6cab608f65d6fbdb44dc04f98ce54f00e4fc78f
ms-joyce <itsjoyce2@gmail.com>
2022-09-20 17:35:42 -0700

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,9 +2,7 @@
 
 /**
  * main - tests function that prints if integer is positive or negative
- *
  * Return: 0
- *
  */
 
 int main(void)
@@ -17,5 +15,4 @@ int main(void)
 	positive_or_negative(i);
 	
 	return (0);
-
 }
