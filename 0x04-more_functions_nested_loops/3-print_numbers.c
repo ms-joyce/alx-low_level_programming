9a7713fc31605dadbff4a8f6ba4f3d5a093dc6c4
ms-joyce <itsjoyce2@gmail.com>
2022-09-22 10:21:07 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print numbers from 0 through 9 folloed by a new line
+ * @i: input
+ * Return: 0 through 9 and new line
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		_putchar(i + '0');
+	_putchar('\n');
+}
