e47f0e877da63b9a87fa3359a8bc1437f8e779c1
ms-joyce <itsjoyce2@gmail.com>
2022-09-22 06:54:12 -0700

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - Return 1 if lowercase and 0 if not
+ *
+ * Return: Always success
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
