cf84925a1797add29322a4a2eadd28a04967d9bc
ms-joyce <itsjoyce2@gmail.com>
2022-09-22 07:56:32 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,11 +11,14 @@ void jack_bauer(void)
 
 	for (h = 0; h < 24; h++)
 	{
-		_putchar((h / 10) + '0');
-		_putchar((h % 10) + '0');
-		_putchar(':');
-		_putchar((m / 10) + '0');
-		_putchar((m % 10) + '0');
-		_putchar('\n');
+		for (m = 0; m < 60; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
 	}
 }
