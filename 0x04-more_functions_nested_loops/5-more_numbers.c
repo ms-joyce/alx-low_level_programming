f508f86b8777a25306a33ac07e41242e0447037e
ms-joyce <itsjoyce2@gmail.com>
2022-09-22 16:42:20 -0700

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,16 +6,14 @@
  */
 void more_numbers(void)
 {
-	if i, j;
-
-	for (i = 0; i = < 10; i++)
+	int i, j;
+	
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 15; j++)
 		{
 			if (j >= 10)
-			{
 				_putchar(j / 10 + '0');
-			}
 			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
