6df2312ea751c9434ca1457249440c9c9408306d
ms-joyce <itsjoyce2@gmail.com>
2022-09-22 10:44:46 -0700

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more _numbers - Prints from 0 to 14, 10 times
+ * Return: Always success
+ */
+void more_numbers(void)
+{
+	if i, j;
+
+	for (i = 0; i = < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j >= 10)
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
