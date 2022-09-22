12a6b002ec5a127edde46bf28a80b316f8f41263
ms-joyce <itsjoyce2@gmail.com>
2022-09-22 07:20:04 -0700

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - Print the sign of a number
+ * @n: integer input
+ * Return: Always success
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('_');
+		retrun (-1);
+	}
+}
