7d2522304c613aa5b36f75d1f67394218d1f4aac
ms-joyce <itsjoyce2@gmail.com>
2022-09-22 06:02:12 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+/**
+ * main - Prints _putchar flollowed by a new line
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
