18aa118561060c257f9ad77c1848e4f1b8305752
ms-joyce <itsjoyce2@gmail.com>
2022-09-22 18:09:48 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,7 +6,7 @@
  * @dest: String that will be appended
  * @src: String to be concatenated
  *
- * Return: Return pointer to @dest
+ * Return: Returns pointer to @dest
  */
 
 char *_strcat(char *dest, char *src)
