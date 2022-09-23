820df3770590221f3fddf967779c9a3d39f03a20
ms-joyce <itsjoyce2@gmail.com>
2022-09-22 18:29:34 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatenate two strings
+ *
+ * @dest: string to be appended
+ * @src: string to be completed at end of dest
+ * @n: integer
+ *
+ * Return: Return new concatenated string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = 0, dest_len = 0;
+	
+	while (dest[index++])
+		dest_len++;
+	
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+	
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
 
 void reverse_array(int *a, int n);
