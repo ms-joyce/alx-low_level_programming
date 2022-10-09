aad568adedd16faad344033769ffe58d1a9f461e
ms-joyce <itsjoyce2@gmail.com>
2022-10-09 15:10:30 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <unistd.h>
 
 /**
- * main - Print text 
+ * main - Print text
  *
  * Return: Always 1 (Succes)
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
